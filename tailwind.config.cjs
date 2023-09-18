3663e467e3e777d85abe0681a4d12beec23196db
Jean Carlos A <carlos.ademir_66@hotmail.com>
2023-09-17 19:51:25 -0500

    Modify the tailwind.config.cjs
    Add plugin for fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,73 @@
 /** @type {import('tailwindcss').Config} */
 
+const plugin = require("tailwindcss/plugin");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        heebo: ["heebo"],
-        montserrat: ["montserrat"],
-        "poppins-300": ["poppins-300"],
-        "poppins-400": ["poppins-400"],
-        "poppins-500": ["poppins-500"],
-        "poppins-700": ["poppins-700"],
+        heebo: ["Heebo", ...defaultTheme.fontFamily.sans],
+        montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Heebo",
+          fontWeight: "800",
+          src: 'url(./src/fonts/Heebo-ExtraBold.woff) format("woff")',
+        },
+      });
+    }),
+    plugin(function ({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Montserrat",
+          fontWeight: "500",
+          src: 'url(./src/fonts/Montserrat-Medium.woff) format("woff")',
+        },
+      });
+    }),
+    plugin(function ({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Poppins",
+          fontWeight: "300",
+          src: 'url(./src/fonts/Poppins-Light.woff) format("woff")',
+        },
+      });
+    }),
+    plugin(function ({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Poppins",
+          fontWeight: "400",
+          src: 'url(./src/fonts/Poppins-Regular.woff) format("woff")',
+        },
+      });
+    }),
+    plugin(function ({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Poppins",
+          fontWeight: "500",
+          src: 'url(./src/fonts/Poppins-Medium.woff) format("woff")',
+        },
+      });
+    }),
+    plugin(function ({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Poppins",
+          fontWeight: "700",
+          src: 'url(./src/fonts/Poppins-Bold.woff) format("woff")',
+        },
+      });
+    }),
+  ],
 };
