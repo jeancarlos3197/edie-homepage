7a4db410e5f575937ccb65aefeb221c492d760a9
Jean Carlos A <carlos.ademir_66@hotmail.com>
2023-09-17 19:06:14 -0500

    Add fonts to astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heebo: ["heebo"],
+        montserrat: ["montserrat"],
+        "poppins-300": ["poppins-300"],
+        "poppins-400": ["poppins-400"],
+        "poppins-500": ["poppins-500"],
+        "poppins-700": ["poppins-700"],
+      },
+    },
+  },
+  plugins: [],
+};
